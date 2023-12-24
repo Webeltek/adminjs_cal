27eb3f43663e4dadcd06e376d742ec3554e77a93
webeltek2017@gmail.com <webeltek2017@gmail.com>
2023-12-24 23:44:20 +0100

    todo fix modal event title useForm handleSubmit form data

--- src/admin/pages/big-cal-page/big-cal-components/pick-time-modal.tsx
+++ src/admin/pages/big-cal-page/big-cal-components/pick-time-modal.tsx
@@ -1,54 +1,67 @@
-import { Box, DatePicker,DatePickerProps, Label, Modal, Input, ModalProps} from '@adminjs/design-system'
+import { Box, DatePicker,DatePickerProps, Label, 
+    Modal, Input, ModalProps, FormGroup} from '@adminjs/design-system'
 import React, { useState, useCallback } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+type eventType = {
+    eventTitle: string
+}
 
 export function PickTimeModal(props){
     const {onOverlayClick,onClose,shModal,start,end,handleModalSaveEvt} = props;
+    const [startDate, setStartDate] = useState<string | null>(start)
+    const [endDate, setEndDate] = useState<string | null>(end)
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<eventType>();
     if (shModal){
         console.log("pick-time-modal shModal,start",shModal,start)
-        const [evtTitle,setEvtTitle]= useState('');
+        const handleStartChange = (value) => {
+            if (value) setStartDate(value)
+            else setStartDate(null)
+        }
+        
+        const handleEndChange = (value) => {
+            if (value) setEndDate(value)
+            else setEndDate(null)
+        }
+        
+        function onSaveEvent(data: eventType){
+            console.log("pick-time-modal data",data)
+            handleModalSaveEvt(data.eventTitle,startDate,endDate)
+        }
         const modalProps: ModalProps = {
             variant: 'primary',
             label: 'Create event',
             icon: 'Calendar',
             title: 'Define event parameters',
             subTitle: 'Insert title',
-            buttons: [{ label: 'Cancel', onClick: onClose }, { label: 'Save', color: 'danger' , onClick: onSaveEvent}],
+            buttons: [
+                { label: 'Cancel', onClick: onClose }, 
+                { label: 'Save', color: 'danger' , onClick: onSaveEvent, type: "submit"}],
             onClose : onClose,
             onOverlayClick : onOverlayClick    
-        }
-        const [startDate, setStartDate] = useState<string | null>(start)
-        const handleStartChange = (value) => {
-            if (value) setStartDate(value)
-            else setStartDate(null)
-        }
-        const [endDate, setEndDate] = useState<string | null>(end)
-        const handleEndChange = (value) => {
-            if (value) setEndDate(value)
-            else setEndDate(null)
-        }
-        function onSaveEvent(){
-            handleModalSaveEvt('alabala',startDate,endDate)
         }
 
         return (
             <Modal {...modalProps}>
-                <Label htmlFor="evtTitle">Inser event title</Label>
-                <Input id="evtTitle" type="text" variant="default"/> 
-                <Box>
-                    <DatePicker
-                        onChange={handleStartChange}
-                        value={startDate ?? ''}
-                        disabled={false}
-                        propertyType='datetime'
-                    />
-                </Box>
-                <Box>
-                    <DatePicker
-                        onChange={handleEndChange}
-                        value={endDate ?? ''}
-                        disabled={false}
-                        propertyType='datetime'
-                    />
+                <Box as="form" noValidate onSubmit={handleSubmit(onSaveEvent)}>
+                    <Label htmlFor="eventTitle">Inser event title</Label>
+                    <Input  id="eventTitle" type="text" variant="default" {...register('eventTitle')}/> 
+                    <Box>
+                        <DatePicker
+                            onChange={handleStartChange}
+                            value={startDate ?? ''}
+                            disabled={false}
+                            propertyType='datetime'
+                        />
+                    </Box>
+                    <Box>
+                        <DatePicker
+                            onChange={handleEndChange}
+                            value={endDate ?? ''}
+                            disabled={false}
+                            propertyType='datetime'
+                        />
+                    </Box>
                 </Box>
             </Modal>
         )
