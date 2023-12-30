494349c5226e10863c4535ab224d2f80a7ebf4d1
webeltek2017@gmail.com <webeltek2017@gmail.com>
2023-12-30 23:34:30 +0100

    todo openModal with selectSlot different start end times

--- src/admin/pages/big-cal-page/big-cal-components/pick-time-form.tsx
+++ src/admin/pages/big-cal-page/big-cal-components/pick-time-form.tsx
@@ -12,49 +12,48 @@ type eventType = {
 
 export function PickTimeModalForm(props){
     const {handleModalSaveEvt} = props;
-    const start = useSelector((state: ReduxState)=> state);
-    const end = useSelector((state: ReduxState)=> state);
-    const [startDate, setStartDate] = useState<string | null>("")
-    const [endDate, setEndDate] = useState<string | null>("")
+    const start = useSelector((state: ReduxState)=> state.event.start);
+    const end = useSelector((state: ReduxState)=> state.event.end);
+    const [startDate, setStartDate] = useState<string | null>(start)
+    const [endDate, setEndDate] = useState<string | null>(end)
     const { register, handleSubmit, watch, formState: { errors } } = useForm<eventType>();
-        console.log("pick-time-form start",start)
-        const handleStartChange = (value) => {
-            if (value) setStartDate(value)
-            else setStartDate(null)
-        }
-        
-        const handleEndChange = (value) => {
-            if (value) setEndDate(value)
-            else setEndDate(null)
-        }
-        
-        function onSaveEvent(data: eventType){
-            console.log("pick-time-modal data.eventTitle",data.eventTitle)
-            handleModalSaveEvt(data.eventTitle,startDate,endDate)
-        }
+    console.log("pick-time-form start,end",start,end)
+    const handleStartChange = (value) => {
+        if (value) setStartDate(value)
+        else setStartDate(null)
+    }
+    
+    const handleEndChange = (value) => {
+        if (value) setEndDate(value)
+        else setEndDate(null)
+    }
+    
+    function onSaveEvent(data: eventType){
+        console.log("pick-time-modal data.eventTitle",data.eventTitle)
+        handleModalSaveEvt(data.eventTitle,startDate,endDate)
+    }
 
-        return (
-            
-                <Box as="form" noValidate onSubmit={handleSubmit(onSaveEvent)}>
-                    <Label htmlFor="eventTitle">Inser event title</Label>
-                    <Input  id="eventTitle" type="text" variant="default" {...register('eventTitle')}/> 
-                    <Box>
-                        <DatePicker
-                            onChange={handleStartChange}
-                            value={startDate ?? ''}
-                            disabled={false}
-                            propertyType='datetime'
-                        />
-                    </Box>
-                    <Box>
-                        <DatePicker
-                            onChange={handleEndChange}
-                            value={endDate ?? ''}
-                            disabled={false}
-                            propertyType='datetime'
-                        />
-                    </Box>
+    return (
+            <Box as="form" noValidate onSubmit={handleSubmit(onSaveEvent)}>
+                <Label htmlFor="eventTitle">Inser event title</Label>
+                <Input  id="eventTitle" type="text" variant="default" {...register('eventTitle')}/> 
+                <Box>
+                    <DatePicker
+                        onChange={handleStartChange}
+                        value={start ?? ''}
+                        disabled={false}
+                        propertyType='datetime'
+                    />
                 </Box>
-        )
+                <Box>
+                    <DatePicker
+                        onChange={handleEndChange}
+                        value={end ?? ''}
+                        disabled={false}
+                        propertyType='datetime'
+                    />
+                </Box>
+            </Box>
+    )
     
 }
